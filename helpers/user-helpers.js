3b98e0b6f71f62653e11f8815ba496f635b659b3
Gayal Melappilly <gayalsunil@gmail.com>
2024-01-31 15:28:52 +0530

    Edit and view Added

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -24,5 +24,27 @@ module.exports = {
                 resolve(data)
             })
         })
+    },
+
+    editDiary: (id, updatedDiary)=>{
+        return new Promise ((resolve,reject)=>{
+            db.get().collection(colleciton.DIARY_COLLECTION).updateOne({_id: new objectId(id)},
+            {
+                $set: {
+                    date: updatedDiary.date,
+                    content: updatedDiary.content,
+                    limitContent: updatedDiary.content.slice(0,60)
+                }
+            }).then((data)=>{
+                resolve(data)
+            })
+        })
+    },
+
+    findDiary: (id)=>{
+        return new Promise ((resolve, reject)=>{
+            let diary = db.get().collection(colleciton.DIARY_COLLECTION).findOne({_id: new objectId(id)})
+            resolve(diary)
+        })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var userHelpers = require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   userHelpers.getAllDiary().then((diary)=>{
-    console.log(diary)
+    // console.log(diary)
     res.render('index', {diary});
   })
 });
@@ -22,26 +22,45 @@ router.post('/compose', (req,res)=>{
   }
 
   userHelpers.addDiary(diary).then((data)=>{
-    console.log("Diray : "+data)
+    // console.log("Diray : "+data)
     res.redirect('/')
   })
 })
 
 router.get('/pages', (req,res)=>{
   userHelpers.getAllDiary().then((diary)=>{
-    console.log(diary)
+    // console.log(diary)
     res.render('pages', {diary});
   })
-})
-
-router.get('/edit', (req,res)=>{
-  res.render('edit')
 })
 
 router.get('/delete/:id', (req,res)=>{
   let id = req.params.id
   userHelpers.removeDiary(id).then((data)=>{
     res.redirect('/pages')
+  })
+})
+
+router.get('/edit/:id', (req,res)=>{
+  let id = req.params.id
+  userHelpers.findDiary(id).then((editDiary)=>{
+    console.log(editDiary)
+    res.render('edit', {editDiary})
+  })
+})
+
+router.post('/edit/:id', (req,res)=>{
+  let id = req.params.id
+  let updatedDiary = req.body
+  userHelpers.editDiary(id, updatedDiary).then((data)=>{
+    res.redirect('/pages')
+  })
+})
+
+router.get('/view/:id', (req,res)=>{
+  let id = req.params.id
+  userHelpers.findDiary(id).then((viewDiary)=>{
+    res.render('view', {viewDiary})
   })
 })
 
